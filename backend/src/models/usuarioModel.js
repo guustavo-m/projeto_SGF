e5a8304ed86cafc36339bdcca17d496725fafcde
manuellapiva <manuellabpiva@gmail.com>
2026-06-23 10:54:42 -0300

    first

--- backend/src/models/usuarioModel.js
+++ backend/src/models/usuarioModel.js
@@ -1,4 +1,5 @@
 const pool = require('../config/database');
+const bcrypt = require('bcrypt');
 
 async function listarTodos() {
   const result = await pool.query(
@@ -24,41 +25,23 @@ async function buscarPorEmail(email) {
   return resultado.rows[0];
 }
 
-async function criar(dados) {
-  const { email, senha } = dados;
-
-  const sql = `
-    INSERT INTO usuarios (email, senha)
-    VALUES ($1, $2)
-    RETURNING *
-  `;
-  
-  const result = await pool.query(
-    sql,
-    [email, senha || "usuarios"]
-  );
-  
-  return result.rows[0];
-}
-
 async function atualizar(id, dados) {
   const { email, senha } = dados;
-  
-  const sql = `
+
+  const senhaHash = await bcrypt.hash(senha, 10);
+
+  const result = await pool.query(
+    `
     UPDATE usuarios
     SET email = $1, senha = $2
     WHERE id = $3
     RETURNING *
-  `;
-  
-  const result = await pool.query(
-    sql,
-    [email, senha, id]
+    `,
+    [email, senhaHash, id]
   );
-  
+
   return result.rows[0] || null;
 }
-
 async function deletar(id) {
   const result = await pool.query(
     'DELETE FROM usuarios WHERE id = $1',
@@ -70,7 +53,7 @@ async function deletar(id) {
 
 module.exports = {
   listarTodos,
-  buscarPorId,
+  buscarPorId,  
   buscarPorEmail,
   criar,
   atualizar,
